f8c05bab49aef65066437897e4cde48fce5ffb11
wangxy0803 <92924469+wangxy0803@users.noreply.github.com>
2022-09-21 11:02:40 +0800

    Update code_1.py

--- code_1.py
+++ code_1.py
@@ -2,12 +2,10 @@
 class Solution:
     def twoSum(self, nums: list[int], target: int) -> list[int]:
         n=len(nums)
+        #数组长度
         for i in range(n):
             for j in range(i+1,n):
+                #只需要和后面的数匹配，前面的数在前一轮中已经匹配过
                 if (nums[i]+nums[j]==target):
                     return[i,j]
         return[]
-
-
-
-
