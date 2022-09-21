0d15bf5571db65bd39a3c0d8627bf913ddf9f4ae
wangxy0803 <92924469+wangxy0803@users.noreply.github.com>
2022-09-21 11:28:06 +0800

    Create code_2.py

---
+++ 1.TwoSum/code_2.py
@@ -0,0 +1,9 @@
+#哈希表
+class Solution:
+    def twoSum(self,nums:list[int],target:int)->list[int]:
+        hashtable=dict()
+        for i,num in enumerate (nums):
+            if target -num in hashtable :
+                return [hashtable [target -num],i]
+            hashtable [nums[i]]=i
+        return[]
