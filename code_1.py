4b05923f272597fa1d553af3c8a6bb9a10916dc7
wangxy0803 <92924469+wangxy0803@users.noreply.github.com>
2022-09-21 10:47:50 +0800

    TwoSum

---
+++ code_1.py
@@ -0,0 +1,13 @@
+#暴力枚举法
+class Solution:
+    def twoSum(self, nums: list[int], target: int) -> list[int]:
+        n=len(nums)
+        for i in range(n):
+            for j in range(i+1,n):
+                if (nums[i]+nums[j]==target):
+                    return[i,j]
+        return[]
+
+
+
+
