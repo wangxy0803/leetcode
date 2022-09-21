bcae9c0405404640d0c71fbc9ef6582bdbc8ab1f
wangxy0803 <92924469+wangxy0803@users.noreply.github.com>
2022-09-21 11:41:41 +0800

    Update code_2.py

--- 1.TwoSum/code_2.py
+++ 1.TwoSum/code_2.py
@@ -2,8 +2,13 @@
 class Solution:
     def twoSum(self,nums:list[int],target:int)->list[int]:
         hashtable=dict()
+        #创建哈希表
         for i,num in enumerate (nums):
+            #对遍历的nums数组标出数据和数据下标
             if target -num in hashtable :
+                #如果匹配成功
                 return [hashtable [target -num],i]
+                #输出下标组合
             hashtable [nums[i]]=i
+            #如果匹配失败，将数组中的数字放入哈希表中
         return[]
